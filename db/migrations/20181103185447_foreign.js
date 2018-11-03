933a82502567871135dac0590784c1e7e8a9a384
Gabriel <ras.vibpositive@gmail.com>
2018-11-03 19:39:46 +0000

    Adds foreign key to table account_notification

--- db/migrations/20181103185447_foreign.js
+++ db/migrations/20181103185447_foreign.js
@@ -5,11 +5,35 @@ exports.up = function (knex, Promise) {
 		table.foreign('account_id').references('account_id').inTable('account');
 		table.foreign('purse_id').references('purse_id').inTable('purse');
 	})
+	.then(() => {
+		return knex.schema.alterTable('account_notification', function (table) {
+			table.primary(['account_id', 'notification_id']);
+
+			table.foreign('account_id').references('account_id').inTable('account');
+			table.foreign('notification_id').references('notification_id').inTable('notification');
+		})
+	})
 };
 
 exports.down = function (knex, Promise) {
-	return knex.schema.alterTable('account_purses', function (table) {
-		table.dropForeign('account_id')
-		table.dropForeign('purse_id')
-	});
+	// return knex.schema.table('account_purses', function (table) {})
+
+	return knex.schema.table('account_purses', function (table) {
+		return table.dropForeign('account_id')
+	})
+	.then(() => {
+		return knex.schema.table('account_purses', function (table) {
+			return table.dropForeign('purse_id')
+		})
+	})
+	.then(() => {
+		return knex.schema.table('account_notification', function (table) {
+			return table.dropForeign('account_id')
+		})
+	})
+	.then(() => {
+		return knex.schema.table('account_notification', function (table) {
+			return table.dropForeign('notification_id')
+		})
+	})
 };
