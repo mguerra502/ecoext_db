e4b679aadbed51925c113bd4b575b743f4a28d26
Gabriel <ras.vibpositive@gmail.com>
2018-11-22 16:17:43 +0000

    Adds support to environment variables

--- knexfile.js
+++ knexfile.js
@@ -3,30 +3,16 @@ module.exports = {
         client: 'mysql2',
         connection: {
             host: '127.0.0.1',
-            user: process.env.DB_USER,
-            password: process.env.DB_PASSWORD,
-            database: 'ecoext_versioned'
-        },
-        migrations: {
-            directory: __dirname + '/db/migrations',
-        },
-        seeds:{
-            directory: __dirname + '/db/seeds'
-        },
-    },
-    production: {
-        client: 'mysql2',
-        connection: {
-            host: '127.0.0.1',
+            port: process.env.ECOEXT_MARIADB_PORT,
             user: 'root',
-            password: '',
-            database: 'ecoext_versioned'
+            password: process.env.ECOEXT_MARIADB_ROOTPASSWORD,
+            database: 'ecoext'
         },
         migrations: {
             directory: __dirname + '/db/migrations',
         },
         seeds:{
             directory: __dirname + '/db/seeds'
-        }
+        },
     }
 }
