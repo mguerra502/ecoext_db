4cf80e2f0b6b83eb5993874a48c0ab765c80b714
Gabriel <ras.vibpositive@gmail.com>
2018-11-30 13:56:55 +0000

    Fixes seeds on account_transactions

--- db/seeds/05_account_transactions.js
+++ db/seeds/05_account_transactions.js
@@ -1,27 +1,15 @@
 /**/
-exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
+exports.seed = function (knex, Promise) {
+
+  const insert = knex('account_transactions')
+    .insert(knex.raw(`(account_id, transaction_id)
+    SELECT a.account_id, t.transaction_id
+    FROM account a, transaction t
+  `))
+
   return knex('account_transactions').del()
-  .then(function () {
-    knex
-    .select()
-    .from('transaction')
-    .whereNotIn('transaction_id', knex.select("transaction_id").from('account_transactions'))
-    .then(function (result) {
-      return result.forEach(transaction => {
-          return knex("account")
-          .select("*")
-          .whereNotIn('account_id', knex.select("account_id").from('account_transactions'))
-          .then(function (account_result) {
-            account_result.forEach(account => {
-              return knex('account_transactions').insert({
-                account_id: account.account_id,
-                transaction_id: transaction.transaction_id
-              })
-            })
-          })
-      });
+    .then(() => {
+      return insert;
     })
-  });
+
 };
-/**/
--- db/seeds/07_account_notifications.js
+++ db/seeds/07_account_notifications.js
@@ -1,17 +1,13 @@
 /**/
 exports.seed = function (knex, Promise) {
 
-  const insert = knex('account_transactions')
-    .insert(knex.raw(`(account_id, transaction_id)
-    SELECT a.account_id, t.transaction_id
-    FROM account a, transaction t
-    WHERE
-    a.account_id NOT IN(SELECT account_id from account_transactions)
-    AND
-    t.transaction_id NOT IN(SELECT transaction_id from account_transactions);
+  const insert = knex('account_notifications')
+    .insert(knex.raw(`(account_id, notification_id)
+    SELECT a.account_id, n.notification_id
+    FROM account a, notification n
   `))
 
-  return knex('account_transactions').del()
+  return knex('account_notifications').del()
   .then(() => {
     return insert;
   })
