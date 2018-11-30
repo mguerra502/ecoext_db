7822beb51192d115fc44192f86ab445d6ea2abe6
Gabriel <ras.vibpositive@gmail.com>
2018-11-30 11:06:16 +0000

    Adds seed account_transaction

--- db/seeds/05_account_transactions.js
+++ db/seeds/05_account_transactions.js
@@ -3,13 +3,27 @@ exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
   return knex('account_transactions').del()
     .then(function () {
-      // Inserts seed entries
-      return true;
-      return knex('account_transactions').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
+      knex.select().from('transaction')
+        .whereNotIn('transaction_id', knex.select("transaction_id").from('account_transactions'))
+        .then(function (result) {
+          result.forEach(transaction => {
+            const getAccount =
+              knex("account")
+              .select("*")
+              .whereNotIn('account_id', knex.select("account_id").from('account_transactions'))
+              .then(function (account_result) {
+                account_result.forEach(account => {
+
+                  return knex('account_transactions').insert({
+                    account_id: account.account_id,
+                    transaction_id: transaction.transaction_id
+                  }).then(function (retprn) {
+                    console.log(retprn)
+                  })
+                })
+              })
+          });
+        })
     });
 };
 /**/
