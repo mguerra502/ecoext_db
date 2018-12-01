d2cf562cecbd9437b1f2cd52e2070bd36ffc4762
Gabriel <ras.vibpositive@gmail.com>
2018-12-01 14:01:58 +0000

    Adds support to env mysql locally for testing

--- knexfile.js
+++ knexfile.js
@@ -1,34 +1,50 @@
 module.exports = {
-    development: {
-        client: 'mysql2',
-        connection: {
-            host: '127.0.0.1',
-            port: process.env.ECOEXT_MARIADB_PORT,
-            user: 'root',
-            password: process.env.ECOEXT_MARIADB_ROOTPASSWORD,
-            database: 'ecoext'
-        },
-        migrations: {
-            directory: __dirname + '/db/migrations',
-        },
-        seeds:{
-            directory: __dirname + '/db/seeds'
-        },
+  development: {
+    client: "mysql2",
+    connection: {
+      host: "127.0.0.1",
+      port: process.env.ECOEXT_MARIADB_PORT,
+      user: "root",
+      password: process.env.ECOEXT_MARIADB_ROOTPASSWORD,
+      database: "ecoext"
     },
-    local: {
-        client: 'mysql2',
-        connection: {
-            host: '127.0.0.1',
-            port: 3306,
-            user: 'gabriel',
-            password: 'Quemsabecala16!',
-            database: 'ecoext'
-        },
-        migrations: {
-            directory: __dirname + '/db/migrations',
-        },
-        seeds: {
-            directory: __dirname + '/db/seeds'
-        },
+    migrations: {
+      directory: __dirname + "/db/migrations"
+    },
+    seeds: {
+      directory: __dirname + "/db/seeds"
     }
-}
+  },
+  local: {
+    client: "mysql2",
+    connection: {
+      host: "127.0.0.1",
+      port: 3306,
+      user: "gabriel",
+      password: "Quemsabecala16!",
+      database: "ecoext"
+    },
+    migrations: {
+      directory: __dirname + "/db/migrations"
+    },
+    seeds: {
+      directory: __dirname + "/db/seeds"
+    }
+  },
+  mysql: {
+    client: "mysql2",
+    connection: {
+      host: "127.0.0.1",
+      port: 3305,
+      user: "root",
+      password: process.env.ECOEXT_MARIADB_ROOTPASSWORD,
+      database: "ecoext"
+    },
+    migrations: {
+      directory: __dirname + "/db/migrations"
+    },
+    seeds: {
+      directory: __dirname + "/db/seeds"
+    }
+  }
+};
