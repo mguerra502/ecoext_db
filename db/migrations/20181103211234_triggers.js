982ab08a445ac4945605dcde4f57f11ab95ff367
Gabriel Oliveira <ras.vibpositive@gmail.com>
2019-03-20 13:02:57 +0000

    Fxes typo

--- db/migrations/20181103211234_triggers.js
+++ db/migrations/20181103211234_triggers.js
@@ -4,15 +4,9 @@ const {
 } = require('../../triggers/triggers')
 
 exports.up = function(knex, Promise) {
-  // return knex.raw(after_insert_on_account)
-  // .then(()=>{
     return knex.raw(after_insert_on_establishment);
-  })
 };
 
 exports.down = function(knex, Promise) {
-  // return knex.raw("DROP TRIGGER IF EXISTS account_AFTER_INSERT;")
-  // .then(()=>{
-    return knex.raw("DROP TRIGGER IF EXISTS establishment_AFTER_INSERT;")
-  });
+   return knex.raw("DROP TRIGGER IF EXISTS establishment_AFTER_INSERT;")
 };
