7a32dc961a389b0128d849fb1a12aa9177b0da42
Gabriel Oliveira <ras.vibpositive@gmail.com>
2019-03-21 18:20:27 +0000

    Updates fields to notNullable on relation keys

--- db/migrations/20190321132443_CreateRelationKeys.js
+++ db/migrations/20190321132443_CreateRelationKeys.js
@@ -4,11 +4,11 @@ exports.up = function(knex, Promise) {
     // ACCOUNT
     .createTable('keys', function (table) {
         table.engine("InnoDB")
-        table.bigIncrements().primary().unique();
-        table.binary('key_aes', 32);
-        table.binary('key_iv', 16);
-        table.string('ip_before', 15);
-        table.specificType('port', 'smallint');
+        table.string('id', 128).primary().unique().notNullable();
+        table.binary('key_aes', 32).notNullable();
+        table.binary('key_iv', 16).notNullable();
+        table.string('ipv4', 15).notNullable();
+        table.specificType('port', 'smallint').notNullable();
         table.timestamps(false, true);
     })
 };
