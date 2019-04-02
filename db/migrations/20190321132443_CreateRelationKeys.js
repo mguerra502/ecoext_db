7c7928e6f356a7b84d765eda5ee9c2cb60365d3f
Gabriel Oliveira <ras.vibpositive@gmail.com>
2019-04-02 18:52:12 +0100

    Adds Relation for Keys and its seed

--- db/migrations/20190321132443_CreateRelationKeys.js
+++ db/migrations/20190321132443_CreateRelationKeys.js
@@ -5,8 +5,8 @@ exports.up = function(knex, Promise) {
     .createTable('keys', function (table) {
         table.engine("InnoDB")
         table.string('id', 128).primary().unique().notNullable();
-        table.binary('key_aes', 32).notNullable();
-        table.binary('key_iv', 16).notNullable();
+        table.string('key_aes', 64).notNullable();
+        table.string('key_iv', 32).notNullable();
         table.string('ipv4', 15).notNullable();
         table.specificType('port', 'smallint').notNullable();
         table.timestamps(false, true);
